f76148e08b1ab3cf7ec5d8e362f6c9611e0febed
Ksenia Poshekhova <kposehova@gmail.com>
2023-11-11 19:59:41 +0300

    Inichial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int deposit = 13_676;
+        int forEveryDeposited = 20;
+
+        System.out.println("Количество бонусов составляет");
+        System.out.println(deposit / forEveryDeposited);
+    }
+}
